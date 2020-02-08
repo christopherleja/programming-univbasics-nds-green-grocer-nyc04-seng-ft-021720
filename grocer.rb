6485156c4535ab94d0e753ed19b2417f5657fd31
Christopher Leja <christopher.leja@gmail.com>
2020-02-08 18:27:10 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -69,8 +69,10 @@ def apply_clearance(cart)
   i = 0
 
   while i < cart.length do
+    
     if cart[i][:clearance]
       cart[i][:price] = (cart[i][:price] - (cart[i][:price] * 0.2)).round(2)
+    
     end
   i += 1
   end
