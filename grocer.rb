aaddb16eee760cd99c7c64f4a2066dd767de8e38
Christopher Leja <christopher.leja@gmail.com>
2020-02-08 18:23:55 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -79,8 +79,8 @@ end
 def checkout(cart, coupons)
  
   consolidated_cart = consolidate_cart(cart)
-  coupons_applied = apply_coupons(consolidated_cart, coupons)
-  final_cart = apply_clearance(coupons_applied)
+  couponed_cart = apply_coupons(consolidated_cart, coupons)
+  final_cart = apply_clearance(couponed_cart)
  
   total = 0
   i = 0
